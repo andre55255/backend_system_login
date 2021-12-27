81c27036033cd200f60a7c4e2ab54892a33f7c96
andre55255 <andre_luiz.b5@outlook.com>
2021-12-26 21:35:00 -0300

    Configurando variavel ambiente de porta para deploy heroku

--- server.js
+++ server.js
@@ -15,4 +15,4 @@ app.use(cors());
 app.use("/user", userRoutes);
 app.use("/product", productRoutes);
 
-app.listen(PORT_SERVER, () => console.log(`Listening in port ${PORT}`));
+app.listen(PORT, () => console.log(`Listening in port ${PORT}`));
